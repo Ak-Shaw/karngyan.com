f52a3c4bb205527f5b07f6e35be6a15268e815ea
Karn <mail@karngyan.com>
2021-06-19 12:58:43 +0530

    nitpick footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -32,7 +32,7 @@
 
         </div>
         <p class="mt-8 text-sm leading-6 text-gray-400 md:mt-0 md:order-1">
-          {{ $config.name }} • dev template by <span class="ceator-preview hover:text-hot-pink text-gray-300">
+          {{ $store.state.name }} • template by <span class="ceator-preview hover:text-hot-pink text-gray-300">
           <a class="hidden md:inline-block" rel="noreferrer" target="_blank" href="https://github.com/karngyan/karngyan.com">
           <creator-preview
             :avatar="avatar"
